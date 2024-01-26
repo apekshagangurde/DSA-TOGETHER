04c23ea56f91a79fc0aa5f33fc46bfa2ffaa9c81
apekshagangurde <100061307+apekshagangurde@users.noreply.github.com>
2024-01-26 22:36:39 +0530

    Create Topological sort.cpp

---
+++ Topological sort.cpp
@@ -0,0 +1,38 @@
+class Solution
+{
+	public:
+	void dfs(int node, int vis[], vector<int>adj[], stack<int>&st)
+	{
+	    vis[node]=1;
+	    for(auto it:adj[node])
+	    {
+	        if(!vis[it])
+	        {
+	            dfs(it,vis,adj,st);
+	        }
+	    }
+	    st.push(node);
+	}
+	vector<int> topoSort(int V, vector<int> adj[]) 
+	{
+	    int vis[V] = {0};
+	    stack<int> st;
+	    
+	    for(int i = 0; i < V; i++)
+	    {
+	        if(!vis[i])
+	        {
+	            dfs(i,vis,adj,st);
+	        }
+	    }
+	    
+	    vector<int> ans;
+	    
+	    while(!st.empty())
+	    {
+	        ans.push_back(st.top());
+	        st.pop();
+	    }
+	    return ans;
+	}
+};
